0bf5c1f523118d24e7cce514db2c1b6bcfa64d80
vamsi612005 <144357835+vamsi612005@users.noreply.github.com>
2023-10-21 15:34:30 +0530

    my changes

---
+++ chatboy/App/Services/ChatFaceData.js
@@ -0,0 +1,38 @@
+export default chatFaceData = [
+    {
+        id: 1,
+        name: 'Atom',
+        image: 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_22_409561b953.png',
+        primary: '#3B96D2',
+        secondary: ''
+    },
+    
+    {
+        id: 2,
+        name: 'Alice',
+        image: 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_21_2e18bb4a61.png',
+        primary: '#E53057',
+        secondary: ''
+    },
+    {
+        id: 3,
+        name: 'Axel',
+        image: 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685678135/chat_1_c7eda483e3.png',
+        primary: '#FFC107',
+        secondary: ''
+    },
+    {
+        id: 4,
+        name: 'Amy',
+        image: 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_18_893d24cebc.png',
+        primary: '#37474F',
+        secondary: ''
+    },
+    {
+        id: 5,
+        name: 'Ada',
+        image: 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_23_211d7370cb.png',
+        primary: '#2473FE',
+        secondary: ''
+    },
+]
